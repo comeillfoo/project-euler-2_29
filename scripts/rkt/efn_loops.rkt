99a74a0c14d6f722ccffc02bc2efa320c7ef5e4c
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-22 22:36:03 +0100

    feat: do-loop

--- scripts/rkt/efn_loops.rkt
+++ scripts/rkt/efn_loops.rkt
@@ -1,11 +1,7 @@
 #lang racket
 
-(define (even-fibonacci-numbers-less-than limit)
-  (let loop ([previous 1] [current 1])
-    (if (>= current limit)
-        (list)
-        (if (even? current)
-            (list* current (loop current (+ previous current)))
-            (loop current (+ previous current))))))
 
-(for/sum ([number (even-fibonacci-numbers-less-than 4000000)]) number)
+(let ([limit 4000000])
+  (do ([sum 0] [previous 1 current] [current 1 (+ previous current)])
+      ((>= current limit) sum)
+      (if (even? current) (set! sum (+ current sum)) (void))))
