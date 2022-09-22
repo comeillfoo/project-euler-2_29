ca563d78fd4657b3404a44e99e48492267f4c0b6
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-22 18:43:03 +0100

    feat: working with streams but it's evaluating too long

--- scripts/rkt/dp_infinite_lists.rkt
+++ scripts/rkt/dp_infinite_lists.rkt
@@ -1,1 +1,14 @@
+#lang racket
 
+(require racket/stream)
+
+;;; removes duplicates in stream
+(define (stream-uniq lst)
+  (if (stream-empty? lst)
+      empty-stream
+      (stream* (stream-first lst)
+               (stream-uniq (stream-filter (lambda (x) (not (equal? x (stream-first lst))))
+                                           (stream-rest lst))))))
+
+(stream-length (stream-uniq (let ([powers (build-list 99 (lambda (n) (+ n 2)))])
+                              (for*/stream ([a powers] [b powers]) (expt a b)))))
