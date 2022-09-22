108ba1c7b149181d1598b80a5ef18ce84a6699eb
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-23 00:11:02 +0100

    fix: use values not length of sequence

--- scripts/rkt/dp_map_generate.rkt
+++ scripts/rkt/dp_map_generate.rkt
@@ -3,4 +3,4 @@
 (let ([numbers (let ([powers (in-inclusive-range 2 100)])
                  (for*/list ([a powers] [b powers])
                    (expt a b)))])
-  (length (map (lambda (x) 1) (remove-duplicates numbers))))
+  (foldl + 0 (map (lambda (x) 1) (remove-duplicates numbers))))
