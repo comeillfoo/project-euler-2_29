70b834886e33b9cd021ab2b9e96989b1ef532a6a
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-23 16:42:06 +0100

    feat: temporaly removed CI; fix: infinite stream forcing

--- scripts/rkt/dp_infinite_lists.rkt
+++ scripts/rkt/dp_infinite_lists.rkt
@@ -2,13 +2,8 @@
 
 (require racket/stream)
 
-;;; removes duplicates in stream
-(define (stream-uniq lst)
-  (if (stream-empty? lst)
-      empty-stream
-      (stream-cons (stream-first lst)
-               (stream-uniq (stream-filter (lambda (x) (not (equal? x (stream-first lst))))
-                                           (stream-rest lst))))))
-
-(stream-length (stream-uniq (let ([powers (build-list 99 (lambda (n) (+ n 2)))])
-                              (for*/stream ([a powers] [b powers]) (expt a b)))))
+(stream-length (for/stream ([i
+                             (remove-duplicates
+                              (let ([powers (in-inclusive-range 2 100)])
+                                (stream->list (for*/stream ([a powers] [b powers]) (expt a b)))))])
+                           i))
