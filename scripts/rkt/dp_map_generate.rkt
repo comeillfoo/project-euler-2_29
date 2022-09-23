0fe2c10dc5010586683078a032f082dfeb903bef
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-23 11:27:18 +0100

    fix: use append instead of flatten

--- scripts/rkt/dp_map_generate.rkt
+++ scripts/rkt/dp_map_generate.rkt
@@ -3,6 +3,6 @@
 (foldl +
        0
        (map (lambda (n) 1)
-            (remove-duplicates (flatten (let ([powers (inclusive-range 2 100)])
+            (remove-duplicates (append* (let ([powers (inclusive-range 2 100)])
                                           (for/list ([a powers])
                                             (map (lambda (n) (expt n a)) powers)))))))
