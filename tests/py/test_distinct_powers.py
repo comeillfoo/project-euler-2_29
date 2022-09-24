6c6cbfc587ade9f9e2c27259e5ef74e9a23c0966
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-24 21:20:12 +0100

    fix: lint warnings for test except for imports

--- tests/py/test_distinct_powers.py
+++ tests/py/test_distinct_powers.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+test module covers some features of
+distinct_powers function
+"""
 import sys
 sys.path.append("../..")
 
@@ -10,24 +14,39 @@ import pytest
 
 @pytest.mark.parametrize("border", range(101))
 def test_empty_set(border: int) -> None:
-  assert set() == distinct_powers((border, border - 1), (border, border - 1))
+    """
+    test covers returning the empty set
+    if there is a short range between borders
+    """
+    assert set() == distinct_powers((border, border - 1), (border, border - 1))
 
 
 
 @pytest.mark.parametrize("border", range(101))
 def test_single_element_set(border: int) -> None:
-  assert {border**border} == distinct_powers((border, border), (border, border))
+    """
+    test covers the correctness of calculating the
+    numbers that stored into resulting set
+    """
+    assert {border**border} == distinct_powers((border, border), (border, border))
 
 
 @pytest.mark.parametrize("border", range(101))
 def test_set_of_couples(border: int) -> None:
-  first = border**border
-  second = border**(border + 1)
-  expected_set = set([border**border, border**(border + 1)])
-  assert expected_set == distinct_powers((border, border), (border, border + 1))
+    """
+    test covers the same as the test_single_element_set
+    but for the couples
+    """
+    expected_set = set([border**border, border**(border + 1)])
+    assert expected_set == distinct_powers((border, border), (border, border + 1))
 
 
 @pytest.mark.parametrize("border", range(101))
 def test_set_of_quads(border: int) -> None:
-  expected_set = set([border**border, border**(border + 1), (border + 1)**border, (border + 1) ** (border + 1)])
-  assert expected_set == distinct_powers((border, border + 1), (border, border + 1))
+    """
+    test covers the same as the test_single_element_set
+    but for the quads
+    """
+    expected_set = set([border**border, border**(border + 1),
+      (border + 1)**border, (border + 1) ** (border + 1)])
+    assert expected_set == distinct_powers((border, border + 1), (border, border + 1))
