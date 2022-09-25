043fe63fee1cb07e9c3db0ff40695f043b704e25
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-25 01:06:25 +0100

    feat: enabled static analysis for racket scripts

--- scripts/rkt/efn_recursion.rkt
+++ scripts/rkt/efn_recursion.rkt
@@ -1,14 +1,15 @@
 #lang racket
 
 (define (even-sum-of-fibonacci-numbers limit)
-  (if (<= limit 1)
-      0
-      (let ([closest_number (let closest-fibonacci-number ([previous 1] [current 1])
-                              (if (>= current limit)
-                                  previous
-                                  (closest-fibonacci-number current (+ previous current))))])
-        (if (even? closest_number)
-            (+ closest_number (even-sum-of-fibonacci-numbers closest_number))
-            (even-sum-of-fibonacci-numbers closest_number)))))
+  (cond
+    [(> limit 1)
+     (let ([closest_number (let closest-fibonacci-number ([previous 1] [current 1])
+                             (if (>= current limit)
+                                 previous
+                                 (closest-fibonacci-number current (+ previous current))))])
+       (if (even? closest_number)
+           (+ closest_number (even-sum-of-fibonacci-numbers closest_number))
+           (even-sum-of-fibonacci-numbers closest_number)))]
+    [else 0]))
 
 (even-sum-of-fibonacci-numbers 4000000)
