0b74f61e9168f7d9edb168970d5a5c57cbf41580
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-25 15:19:40 +0100

    fix: formatting for racket/tests

--- tests/rkt/efn-infinite-lists-test.rkt
+++ tests/rkt/efn-infinite-lists-test.rkt
@@ -3,4 +3,6 @@
 (require rackunit
          "../../scripts/rkt/efn-infinite-lists.rkt")
 
-(check-equal? (stream-fold + 0 (even-fibonacci-numbers-less-than 4000000)) 4613732 "Exact task solution")
+(check-equal? (stream-fold + 0 (even-fibonacci-numbers-less-than 4000000))
+              4613732
+              "Exact task solution")
